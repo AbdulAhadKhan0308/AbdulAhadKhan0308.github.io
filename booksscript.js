702887b04ae1fdf2056c284582836b5c28a9d385
Abdul Ahad Khan <aahad6674@gmail.com>
2021-04-18 20:52:19 +0530

    js

---
+++ booksscript.js
@@ -0,0 +1,13 @@
+/*"use strict";
+console.log("h");
+fetch("https://gadgets.ndtv.com/news")
+  .then((response) => {
+    console.log("then");
+    response.json().then((data) => {
+      console.log(data);
+    });
+  })
+  .catch(() => {
+    console.log("Error in fetch");
+  });
+*/
